ab52e7ca52819c51d9c9e06032053f0fe2521305
Andrew Akl <andyakel@outlook.com>
2026-09-17 15:19:57 +0300

    Course and student objects

--- CourseSystem/Program.cs
+++ CourseSystem/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Creating course and student objects 
+
+Course math = new Course("Matematik", 2);
+Course programming = new Course("Programmering", 3);
+
+Student anna = new Student("Anna");
+Student erik = new Student("Erik");
+Student sara = new Student("Sara");
+
