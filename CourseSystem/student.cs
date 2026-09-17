19db94ea5b6b2c3cb1a56e8d48abeeff4bd70988
Andrew Akl <andyakel@outlook.com>
2026-09-17 09:56:15 +0300

    gather all logic for joining

--- CourseSystem/student.cs
+++ CourseSystem/student.cs
@@ -12,6 +12,29 @@ public class Student
     }
 
     public void Join(Course course)
+    {
+        // Let Course.Enroll Controll both lists
+        course.Enroll(this);
+    }
 
+    public void Leave(Course course)
+    {
+        course.Remove(this);
+    }
+
+    public void Schedule()
+    {
+        Console.WriteLine($"{Name}s kurser:");
+
+        foreach (Course course in Courses)
+        {
+            Console.WriteLine($"- {course}");
+        }
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
 
 }
