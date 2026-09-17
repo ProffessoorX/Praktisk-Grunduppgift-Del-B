c5e74995475cf6a8b1ec450fd26d48e6534c8520
Andrew Akl <andyakel@outlook.com>
2026-09-17 09:53:05 +0300

    Created student class

---
+++ CourseSystem/student.cs
@@ -0,0 +1,17 @@
+public class Student
+
+// Creating student class
+
+{
+    public string Name;
+    public List<Course> Courses = new List<Course>();
+
+    public Student(string name)
+    {
+        Name = name;
+    }
+
+    public void Join(Course course)
+
+
+}
